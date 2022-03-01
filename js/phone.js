410c8ce994e4cb19b9df97b1de3199f42cc6be15
sheikhsumaya <sheikhsumaya622@gmail.com>
2022-03-02 01:12:29 +0600

    five

--- js/phone.js
+++ js/phone.js
@@ -3,30 +3,22 @@
   const searchPhone = () =>{
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
+
   // console.log(searchText);
  
 // clear data 
   searchField.value = '';
-if(searchText ==''){
+  if(searchText ==''){
   alert('Please write somethings to show!!')
-}
-else{
+  }
+  else{
   // search function  
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   // console.log(url);
   fetch(url)
   .then(res => res.json())
-  .then(result => displaySearchResult(result.data));
-}
-
-}
-// // search function  
-//   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-//   // console.log(url);
-//   fetch(url)
-//   .then(res => res.json())
-//   .then(result => displaySearchResult(result.data));
-// }
+  .then(result => displaySearchResult(result.data.slice(0,20)));
+  }}
 
 //data display result 
 
@@ -38,6 +30,8 @@ else{
 
   if(data.length == 0){
     alert("Sorry! There is no matched phone.")
+   
+
   }
   else{
 // for loop 
@@ -49,39 +43,21 @@ else{
   // html card 
   
       div.innerHTML = `
+      <div class=" mx-auto p-3 w-75 rounded border border-3 mb-5">
       <div onclick="loadDataDetail('${data.slug}')" class="card h-100">
-         <img src="${data.image}" class="card-img-top img-fluid p-5 w-75 mx-auto my-5 rounded"  alt="...">
+         <img src="${data.image}" class="card-img-top img-fluid p-5 rounded"  alt="...">
           <div class="card-body">
-           <h5 class="card-title">${data.phone_name}</h5>
-             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <h3 class="class-head">Brand:${data.brand}</h3>
+           <h5 class="card-title">Name:${data.phone_name}</h5>
              <button onClick="" class="btn btn-outline-secondary" type="button" id="button-details">Details</button>
           </div>
+      </div> 
       </div>`;
       searchResult.appendChild(div);
     })
   }}
-  
 
-// // for loop 
-//   data.forEach(data => {
-//   // console.log(data)
-//   const div = document.createElement('div');
-//   div.classList.add('col');
 
-// // html card 
-
-//     div.innerHTML = `
-//     <div onclick="loadDataDetail('${data.slug}')" class="card h-100">
-//        <img src="${data.image}" class="card-img-top img-fluid p-5 w-75 mx-auto my-5 rounded"  alt="...">
-//         <div class="card-body">
-//          <h5 class="card-title">${data.phone_name}</h5>
-//            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//            <button onClick="" class="btn btn-outline-secondary" type="button" id="button-details">Details</button>
-//         </div>
-//     </div>`;
-//     searchResult.appendChild(div);
-//   })
-// }
 
 //  loading details 
  
@@ -94,7 +70,6 @@ else{
 }
 
 //card display details 
-
   const displayPhoneDetails = data => {
   // console.log(data);
   const phoneDetails = document.getElementById('phone-details');
@@ -106,13 +81,30 @@ else{
   const div = document.createElement('div');
   div.classList.add('card');
   div.innerHTML = `
+  <div class=" mx-auto p-3 w-75 rounded border border-3 my-5">
   <img src="${data.image}" class="card-img-top img-fluid p-5 w-75 mx-auto rounded" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${data.phone_name}</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Find Another One</a>
+  <div class="card-body border border-success p-4">
+    <h3 class="card-title">Brand:${data.brand}</h3>
+    <h4 class="card-title">Name:${data.name}</h4>
+    <h5 class="card-title">Release Date:${data.releaseDate}</h5>
+    <p class="card-text">Storage:${data.mainFeatures.storage}</p>
+    <p class="card-text">Display Size:${data.mainFeatures.displaySize}</p>
+    <p class="card-text">Chipset:${data.mainFeatures.chipSet}</p>
+    <p class="card-text">Memory:${data.mainFeatures.memory}</p>
+    <p class="card-text ">Sensors:${data.mainFeatures.sensors[0]}</p>
+    <p class="card-text">WLAN:${data.others.WLAN}</p>
+    <p class="card-text">Bluetooth:${data.others.Bluetooth}</p>
+    <p class="card-text ">GPS:${data.others.GPS}</p>
+    <p class="card-text ">NFC:${data.others.NFC}</p>
+    <p class="card-text">Radio:${data.others.Radio}</p>
+    <p class="card-text ">USB:${data.others.USB}</p>
+
+    <a href="#" class="btn btn-outline-secondary">Find Another One</a>
   </div> 
+  </div>
   `;
   phoneDetails.appendChild(div);
+
+
 }
 
